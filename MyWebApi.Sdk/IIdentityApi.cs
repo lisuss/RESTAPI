b124369336e1b39acbf06abdbf026df2a9feb6e5
lisuss <55858498+lisuss@users.noreply.github.com>
2020-06-07 14:52:09 +0200

    Add files via upload

---
+++ MyWebApi.Sdk/IIdentityApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Refit;
+using System.Text;
+using System.Threading.Tasks;
+using MyWebApi.Contracts.V1.Responses;
+using MyWebApi.Contracts.V1.Requests;
+
+namespace MyWebApi.Sdk
+{
+    public interface IIdentityApi
+    {
+        [Post("/api/v1/identity/register")]
+        Task<ApiResponse<AuthSuccessResponse>> RegisterAsync([Body]UserRegistrationRequest registrationRequest);
+
+        [Post("/api/v1/identity/login")]
+        Task<ApiResponse<AuthSuccessResponse>> LoginAsync([Body]UserLoginRequest userLoginRequest);
+
+        [Post("/api/v1/identity/refresh")]
+        Task<ApiResponse<AuthSuccessResponse>> RefreshAsync([Body]RefreshTokenRequest refreshTokenRequest);
+    }
+}
---
+++ MyWebApi.Sdk/IMyWebApi.cs
@@ -0,0 +1,29 @@
+﻿using MyWebApi.Contracts.V1.Requests;
+using MyWebApi.Contracts.V1.Responses;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Sdk
+{
+    [Headers("Authorization: Bearer")]
+    public interface IMyWebApi
+    {
+        [Get("/api/v1/posts")]
+        Task<ApiResponse<List<PostResponse>>> GetAllAsync();
+
+        [Get("/api/v1/posts/{postid}")]
+        Task<ApiResponse<PostResponse>> GetAsync(Guid postId);
+
+        [Post("/api/v1/posts")]
+        Task<ApiResponse<PostResponse>> CreateAsync([Body] CreatePostRequest createPostRequest);
+
+        [Put("/api/v1/posts/{postId}")]
+        Task<ApiResponse<PostResponse>> UpdateAsync(Guid postId, [Body] UpdatePostRequest updateePostRequest);
+
+        [Delete("/api/v1/posts/{postId}")]
+        Task<ApiResponse<string>> DeleteAsync(Guid postId);
+    }
+}
